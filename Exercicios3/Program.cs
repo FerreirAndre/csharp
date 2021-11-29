1d34ffc188c951dc88013ee3dd0b6d17c1705264
FerreirAndre <adrecruz15@gmail.com>
2021-11-29 16:33:35 -0300

    listas de exercicios 3 e 4

---
+++ Exercicios3/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            var p = new Program();
+            p.Ex1();
+            p.Ex2();
+            p.Ex3();
+        }
+
+        public void Ex1()
+        {
+            int senha;
+            Console.Write("digite a senha: ");
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("senha invalida");
+                Console.Write("Digite a senha: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+            System.Console.WriteLine("Senha correta");
+        }
+        public void Ex2()
+        {
+            int x, y;
+            Console.Write("Digite as coordenadas: ");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            while (x != 0 || y != 0)
+            {
+                if (x > 0 && y > 0)
+                {
+                    System.Console.WriteLine("Primeiro quadrante");
+                }
+                else if (x < 0 && y > 0)
+                {
+                    System.Console.WriteLine("Segundo quadrante");
+                }
+                else if (x < 0 && y < 0)
+                {
+                    System.Console.WriteLine("Terceiro quadrante");
+                }
+                else
+                {
+                    System.Console.WriteLine("Quarto Quadrante");
+                }
+                Console.Write("Digite as coordenadas: ");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+            }
+        }
+        public void Ex3()
+        {
+            int opcao, alcool = 0, gasolina = 0, diesel = 0;
+            System.Console.WriteLine("Digite os combustiveis de preferencia\n1-alcool\n2-gasolina\n3-diesel");
+            System.Console.Write("digite a opcao: ");
+            opcao = int.Parse(Console.ReadLine());
+
+            while (opcao != 4)
+            {
+                switch (opcao)
+                {
+                    case 1:
+                        alcool++;
+                        break;
+                    case 2:
+                        gasolina++;
+                        break;
+                    case 3:
+                        diesel++;
+                        break;
+                    default:
+                        System.Console.WriteLine("opcao invalida");
+                        break;
+                }
+                System.Console.Write("Digite o combustivel: ");
+                opcao = int.Parse(Console.ReadLine());
+            }
+            System.Console.WriteLine($"muito obrigado!\nalcool = {alcool}\ngasolina = {gasolina}\ndiesel = {diesel}");
+
+        }
+    }
+}
---
+++ exercicio4/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            var p = new Program();
+            int opcao;
+            do
+            {
+                Console.Write("digite qual exercicio deseja fazer: ");
+                opcao = int.Parse(Console.ReadLine());
+                switch (opcao)
+                {
+                    case 1:
+                        p.Ex1();
+                        break;
+                    case 2:
+                        p.Ex2();
+                        break;
+                    case 3:
+                        p.Ex3();
+                        break;
+                    case 4:
+                        p.Ex4();
+                        break;
+                    case 5:
+                        p.Ex5();
+                        break;
+                    case 6:
+                        p.Ex6();
+                        break;
+                    case 7:
+                        p.Ex7();
+                        break;
+                    default:
+                        break;
+                }
+            } while (opcao != 0);
+        }
+        public void Ex1()
+        {
+            Console.Write("Digite um numero para saber os impares: ");
+
+            var valor = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= valor; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        public void Ex2()
+        {
+            Console.Write("Digite a qtd de numeros a serem lidos: ");
+            var qtd = int.Parse(Console.ReadLine());
+            int valor, dentro = 0, fora = 0;
+            for (int i = 0; i < qtd; i++)
+            {
+                valor = int.Parse(Console.ReadLine());
+                if (valor >= 10 && valor <= 20)
+                    dentro++;
+                else
+                    fora++;
+            }
+            Console.WriteLine($"{dentro} in\n{fora} out.");
+        }
+        public void Ex3()
+        {
+            Console.Write("Digite a quantidade de testes: ");
+            var qtd = int.Parse(Console.ReadLine());
+            double num1, num2, num3, media;
+            for (int i = 0; i < qtd; i++)
+            {
+                Console.Write($"valor {i + 1}:");
+                num1 = double.Parse(Console.ReadLine());
+                num2 = double.Parse(Console.ReadLine());
+                num3 = double.Parse(Console.ReadLine());
+                media = num1 * 0.2 + num2 * 0.3 + num3 * 0.5;
+                Console.WriteLine(media.ToString("F1"));
+            }
+        }
+        public void Ex4()
+        {
+            Console.Write("digite a quantidade de contas: ");
+            int qtd = int.Parse(Console.ReadLine());
+            for (int i = 0; i < qtd; i++)
+            {
+                Console.Write("Digite dois numeros para fazer a divisao: ");
+                var s = Console.ReadLine().Split(' ');
+                double num1 = double.Parse(s[0]);
+                double num2 = double.Parse(s[1]);
+                if (num2 == 0.0)
+                {
+                    Console.WriteLine("operacao impossivel.");
+                }
+                else
+                {
+                    Console.WriteLine(num1 / num2);
+                }
+            }
+        }
+        public void Ex5()
+        {
+            Console.Write("digite um numero para saber seu fatorial: ");
+            int valor = int.Parse(Console.ReadLine());
+            int resultado = 1;
+            if (valor != 0)
+            {
+                for (int i = valor; i > 0; i--)
+                {
+                    resultado *= i;
+                }
+            }
+            Console.WriteLine(resultado);
+        }
+        public void Ex6()
+        {
+            Console.Write("digite um numero para calcular seus divisores: ");
+            int valor = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= valor; i++)
+            {
+                if (valor % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        public void Ex7()
+        {
+            Console.Write("digite um numero: ");
+            int valor = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[valor, 3];
+
+            for(int i = 1; i <= valor; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    if (j == 0)
+                    {
+                        matriz[i-1, j] = i;
+                    }
+                    else if (j == 1)
+                    {
+                        matriz[i-1, j] = (int)Math.Pow(i,2);
+                    }
+                    else
+                    {
+                        matriz[i - 1, j] = (int)Math.Pow(i, 3);
+                    }
+                    Console.Write($"{matriz[i-1,j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
